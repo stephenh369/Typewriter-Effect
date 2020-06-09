cd46b16a0a4962acaa7cf9e8e9db837be410f08f
Stephen <stephen_h369@live.co.uk>
2020-06-09 20:25:48 +0100

    added solid cursor when typing or deleting

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 const typingSpan = document.querySelector(".typing");
+const cursor = document.querySelector(".cursor");
+const solidCursor = document.querySelector(".solidCursor");
 const typeText = ["code.", "drive.", "listen to music.", "go to the gym.", "play guitar."];
 const typingDelay = 200;
 const deleteDelay = 100;
 const newTextDelay = 2000;
 let textIndex = 0;
 let letterIndex = 0;
+solidCursor.classList.remove("solidCursor");
 
 function type() {
-   if (letterIndex < typeText[textIndex].length) {
+    cursor.classList.remove("cursor");
+    solidCursor.classList.add("solidCursor");
+    if (letterIndex < typeText[textIndex].length) {
     typingSpan.textContent += typeText[textIndex].charAt(letterIndex);
     letterIndex++;
     setTimeout(type, typingDelay);
    } else {
        setTimeout(delText, newTextDelay);
+       solidCursor.classList.remove("solidCursor");
+       cursor.classList.add("cursor");
    }
 };
 
 function delText() {
+    cursor.classList.remove("cursor");
+    solidCursor.classList.add("solidCursor");
     if (letterIndex > 0) {
         typingSpan.textContent = typeText[textIndex].substring(0, letterIndex - 1);
         letterIndex--;
         setTimeout(delText, deleteDelay);
     } else {
         textIndex++;
+        solidCursor.classList.remove("solidCursor");
+       cursor.classList.add("cursor");
         if (textIndex >= typeText.length)
             textIndex = 0;
             setTimeout(type, typingDelay + 1000);
+            
     }
 };
 
